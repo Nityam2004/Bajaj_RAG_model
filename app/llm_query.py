0eb846f2d1520201c3f261dca53f67cd2a57e50a
NITYAM <122718944+Nityam2004@users.noreply.github.com>
2025-08-05 14:03:51 +0530

    Update llm_query.py

--- app/llm_query.py
+++ app/llm_query.py
@@ -7,7 +7,7 @@ if not GOOGLE_API_KEY:
     raise ValueError("GOOGLE_API_KEY environment variable is not set")
 
 def get_answer(vectorstore, question: str):
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=GOOGLE_API_KEY)
+    llm = ChatGoogleGenerativeAI(model="Gemini 2.0 Flash-Lite", google_api_key=GOOGLE_API_KEY)
     qa = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever())
     result = qa.run(question)
     return result
